65edcda7546296a5aead6d9b849143488af710f9
skuzzis <andreieurt@gmail.com>
2026-03-21 23:35:52 +0200

    fix(SSClient): Double definition

--- managed/src/SwiftlyS2.Shared/Natives/Structs/CServerSideClientBase.cs
+++ managed/src/SwiftlyS2.Shared/Natives/Structs/CServerSideClientBase.cs
@@ -5,22 +5,6 @@ using SwiftlyS2.Shared.SteamAPI;
 namespace SwiftlyS2.Shared.Natives;
 
 [StructLayout(LayoutKind.Sequential)]
-public struct Spike_t
-{
-    public CUtlString Desc;
-    public int Bits;
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct CNetworkStatTrace
-{
-    public CUtlVector<Spike_t> Records;
-    public int MinWarningBytes;
-    public int StartBit;
-    public int CurrentBit;
-}
-
-[StructLayout(LayoutKind.Sequential)]
 public unsafe struct CServerSideClientBase
 {
     private fixed byte IdkPadding[0x48];
