be61305ca74fe8b9424b2403c83837295b0d068a
skuzzis <andreieurt@gmail.com>
2026-04-25 14:29:33 +0300

    feat(player/components): ToPlayer

--- managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CPlayerControllerComponent.cs
+++ managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CPlayerControllerComponent.cs
@@ -1,4 +1,6 @@
-﻿namespace SwiftlyS2.Shared.SchemaDefinitions;
+﻿using SwiftlyS2.Shared.Players;
+
+namespace SwiftlyS2.Shared.SchemaDefinitions;
 
 public partial interface CPlayerControllerComponent
 {
@@ -6,4 +8,9 @@ public partial interface CPlayerControllerComponent
     /// Gets the player controller associated with this instance.
     /// </summary>
     public CBasePlayerController Controller { get; }
+
+    /// <summary>
+    /// Attempts to convert this component's pawn to an <see cref="IPlayer"/> instance.
+    /// </summary>
+    public IPlayer? ToPlayer();
 }
--- managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CPlayerPawnComponentImpl.cs
+++ managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CPlayerPawnComponentImpl.cs
@@ -1,8 +1,14 @@
-﻿using SwiftlyS2.Shared.SchemaDefinitions;
+﻿using SwiftlyS2.Shared.Players;
+using SwiftlyS2.Shared.SchemaDefinitions;
 
 namespace SwiftlyS2.Core.SchemaDefinitions;
 
 internal partial class CPlayerPawnComponentImpl : CPlayerPawnComponent
 {
     public CBasePlayerPawn Pawn => __m_pChainEntity.Entity.As<CBasePlayerPawn>();
+
+    public IPlayer? ToPlayer()
+    {
+        return Pawn.ToPlayer();
+    }
 }
